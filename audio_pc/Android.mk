e8179e768dd364c44a6725bea97441d90487c07b
Viorel Suman <viorel.suman@intel.com>
2014-03-31 11:52:24 +0300

    Added support for ALC283 chipset
    NUC audio: added support for ALC283 chipset
    
    Change-Id: Ia99f7b9af74b22b3ab788034468654fabbad3e8e
    Signed-off-by: Viorel Suman <viorel.suman@intel.com>
    Reviewed-on: https://android.intel.com/176457
    Reviewed-by: Chiras, Robert <robert.chiras@intel.com>
    Tested-by: Vlaescu, CristianX <cristianx.vlaescu@intel.com>
    Reviewed-by: Serban, Mihai <mihai.serban@intel.com>

--- audio_pc/Android.mk
+++ audio_pc/Android.mk
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+ifeq ($(AUDIO_HAL), audio_pc)
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -30,4 +31,5 @@ LOCAL_SHARED_LIBRARIES := liblog libcutils libtinyalsa libaudioutils libexpat
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
+endif #AUDIO_HAL
 
